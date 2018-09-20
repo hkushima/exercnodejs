eb3704ac4e58cd738f097a757d6f2b8cc16b1b54
Henrique <henrique.kushima@gmail.com>
2018-09-20 19:12:40 -0300

    Aula 2
    exerc 1 e 2

--- aula2_exerc1.js
+++ aula2_exerc1.js
@@ -3,3 +3,25 @@
  * Procurar no array e printar na tela se achou ou não achou.
  */
 
+let arr = [];
+let i = 0;
+while (i<=100){
+    
+    arr[i] = i;
+    i++;
+
+}
+
+var prompt = require('prompt-sync')();
+var valor = prompt('Digite um número: ');
+let naoachei = true;
+for (let j in arr){
+
+    if (valor == arr[j]) {
+        naoachei=false;
+        break;
+    }
+}
+
+if (naoachei) console.log('não achei');
+else console.log('achei');
--- aula2_exerc2.js
+++ aula2_exerc2.js
@@ -3,4 +3,28 @@
  * Imprimir o número que está faltando no array (usando o exemplo acima, o programa printaria o 5).
  */
 
+//var prompt = require('prompt-sync')();
+//var valor = prompt('Valor pra pular: ');
+let valor = 5;
 
+let arr = [];
+let i = 1;
+arr[0] = 0
+while (i<9){
+    
+    if (i == valor) arr[i] = arr[i-1]+2
+    else arr[i] =  arr[i-1]+1;
+    
+    i++;
+    
+}
+
+console.log(arr)
+
+for (let j=0;j<arr.length;j++){
+
+    if (j != arr[j]) {
+        console.log("O valor que está faltando é "+j)
+        break;
+    }
+}
