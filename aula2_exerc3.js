2e43b43b44a73abbb441f1b72d227d5bc72c420a
Henrique <henrique.kushima@gmail.com>
2018-09-20 19:54:47 -0300

    aula 2 - exerc 3
    exercicio 3

--- aula2_exerc3.js
+++ aula2_exerc3.js
@@ -9,3 +9,42 @@
         - Caso o usuário digitar outra coisa (não precisa validar se o valor digitado é um número), repita todos os passos novamente
 
  */
+
+var prompt = require('prompt-sync')();
+
+let inform, conta, resp, num1, num2, operac, res;
+do {
+   
+    for (let i=0;i<3;i++){
+
+            if (i == 0) num1 = prompt('Favor informar um numero: ');
+            else if (i == 1) operac = prompt('Favor informar uma operação (soma, subtracao, multiplicacao, divisao): ');
+            else num2 = prompt('Favor informar outro numero: ');
+    }
+    switch (operac){
+
+        case "soma":
+            res = parseFloat(num1)+parseFloat(num2);
+            console.log("Resultado: "+res);
+            break;
+        case "subtracao":
+            res = parseFloat(num1)-parseFloat(num2);
+            console.log("Resultado: "+res);
+            break;    
+        case "multiplicacao":
+            res = parseFloat(num1)*parseFloat(num2);
+            console.log("Resultado: "+res);
+            break;
+        case "divisao":
+            res = parseFloat(num1)/parseFloat(num2);
+            console.log("Resultado: "+res);
+            break;
+    }
+    
+    
+    //resp = prompt('Para fazer outra operação digite um número ou digite "sair" para finalizar o programa..');
+    resp = prompt('Digite "sair" para finalizar o programa ou tecle ENTER para iniciar novamente..');
+
+} while (resp != "sair")
+
+
